b814ad3f45debe189a3997320289e4a3c5ebe2b3
Sachin Fegade <sachin.fegade@copart.com>
2020-05-25 19:11:46 -0500

    AssertJsonArray completed - uses thread pools and streamsupport

--- src/main/java/org/planet/Json/jsonUtils/exceptions/ExceedsLimitOfObjectSize.java
+++ src/main/java/org/planet/Json/jsonUtils/exceptions/ExceedsLimitOfObjectSize.java
@@ -1,7 +1,7 @@
 package org.planet.Json.jsonUtils.exceptions;
 
 public class ExceedsLimitOfObjectSize extends Exception {
-
+    //thrown only for JsonObject
     public ExceedsLimitOfObjectSize() {
         super("Json Object or Json Array exceed the size allowed for assertion.");
     }
